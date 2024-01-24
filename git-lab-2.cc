e4b58ccc67f56321d99739f0e824171798af03a9
Benjaminjuedes <bj249422@ohio.edu>
2024-01-23 20:12:15 -0500

    git lab 2 Benjamin Juedes

---
+++ git-lab-2.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: benjamin Juedes
+ *        Date: January 23rd
+ * Description: Git lab 2
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+cout << "Hello Git!!" << endl;
+    /*add code*/
+    return 0;
+}// main
